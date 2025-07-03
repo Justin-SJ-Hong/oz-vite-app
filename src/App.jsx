323da6f3229f674b19744d9668c01e576f6037d9
Justin-SJ-Hong <madwolves98@gmail.com>
2025-07-03 10:44:24 +0900

    조건부 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,112 @@ import './App.css'
 import { Component } from 'react'
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [inputValue, setInputValue] = useState(1)
-
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-  }
-
-  const decrementCounter = () => {
-    setCounter(counter - 1);
-  }
-
-  const setCounterNumber = () => {
-    setCounter(inputValue);
-  }
-
+  const [mood, setMood] = useState('Normal');
   return (
     <>
-      {/* <Count counter={counter} hello={"hello"} array={[1, 2, 3, '안녕하세요.']} /> */}
-      <Count counter={counter} />
-      <PlusButton setCounter={setCounter} incrementCounter={incrementCounter} />
-      <MinusButton setCounter={setCounter} decrementCounter={decrementCounter} />
-      <CounterInput inputValue={inputValue} setInputValue={setInputValue} setCounterNumber={setCounterNumber} />
+      <Face3 mood={mood} />
+      <div className={mood === "Happy" ? 'happy' : mood === "Normal" ? 'normal' : 'sad'}>기분 : {mood}</div>
+      <div>
+        <button onClick={() => setMood("Happy")}>Happy</button>
+        <button onClick={() => setMood("Normal")}>Normal</button>
+        <button onClick={() => setMood("Sad")}>Sad</button>
+      </div>
+    </>
+  );
+}
+
+
+// If 문으로 리턴하는 JSX문
+function Face ({mood}) {
+  if (mood === 'Happy') {
+    return <div>😊</div>
+  } else if(mood === 'Normal') {
+    return <p>😑</p>
+  } else {
+    return <span>😭</span>
+  }
+}
+
+// 삼항연산자 사용
+function Face2({mood}) {
+  return (
+    <>
+      {mood === "Happy" ? (
+        <div>😊</div>
+      ) : mood === "Normal" ? (
+        <p>😑</p> 
+      ) : (
+        <span>😭</span>
+      )}
     </>
   )
 }
 
-function CounterInput( { inputValue, setInputValue, setCounterNumber } ) {
+// 논리 연산자 사용
+function Face3({mood}) {
   return (
     <>
-      <input type='number' value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
-      <button onClick={setCounterNumber}>입력</button>
+      {mood === "Happy" && <div>😊</div>}
+      {mood === "Normal" && <p>😑</p>}
+      {mood === "Sad" && <span>😭</span>}
     </>
   )
 }
 
-function PlusButton({setCounter, incrementCounter}) {
-  return <button onClick={incrementCounter}>+</button>;
-}
+export default App;
 
-function MinusButton({setCounter, decrementCounter}) {
-  return <button onClick={decrementCounter}>-</button>;
-}
+// function App() {
+//   const [counter, setCounter] = useState(0)
+//   const [inputValue, setInputValue] = useState(1)
 
-export default App
+//   const incrementCounter = () => {
+//     setCounter(counter + 1);
+//   }
+
+//   const decrementCounter = () => {
+//     setCounter(counter - 1);
+//   }
+
+//   const setCounterNumber = () => {
+//     setCounter(inputValue);
+//   }
+
+//   return (
+//     <>
+//       {/* <Count counter={counter} hello={"hello"} array={[1, 2, 3, '안녕하세요.']} /> */}
+//       <Count counter={counter} />
+//       <PlusButton setCounter={setCounter} incrementCounter={incrementCounter} />
+//       <MinusButton setCounter={setCounter} decrementCounter={decrementCounter} />
+//       <CounterInput inputValue={inputValue} setInputValue={setInputValue} setCounterNumber={setCounterNumber} />
+//     </>
+//   )
+// }
+
+// function CounterInput( { inputValue, setInputValue, setCounterNumber } ) {
+//   return (
+//     <>
+//       <input type='number' value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+//       <button onClick={setCounterNumber}>입력</button>
+//     </>
+//   )
+// }
+
+// function PlusButton({setCounter, incrementCounter}) {
+//   return <button onClick={incrementCounter}>+</button>;
+// }
+
+// function MinusButton({setCounter, decrementCounter}) {
+//   return <button onClick={decrementCounter}>-</button>;
+// }
+
+// export default App
+
+// function Count ({array, counter, hello}) {
+//   // console.log('array', array);
+//   // console.log('counter', counter);
+//   // console.log('hello', hello);
+//   return <div>Counter : {counter}</div>
+// }
 
 // class ClassApp extends Component {
 //   state = {counter: 1};
@@ -71,13 +133,6 @@ export default App
 //     )
 //   }
 // }
-
-function Count ({array, counter, hello}) {
-  // console.log('array', array);
-  // console.log('counter', counter);
-  // console.log('hello', hello);
-  return <div>Counter : {counter}</div>
-}
 
 // class PlusButton extends Component {
 //   render() {
